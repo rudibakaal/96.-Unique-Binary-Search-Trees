3540a713e5ab3129c1c0462911ee8ec128fe0e86
Rudi Bakaal <48378196+rudibakaal@users.noreply.github.com>
2021-09-27 19:21:05 +1000

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,9 +1,6 @@
 class Solution(object):
     def numTrees(self, n):
-        """
-        :type n: int
-        :rtype: int
-        """
+        import math 
         numer = math.factorial(2*n)
         denom = math.factorial(n+1)*math.factorial(n)
 
